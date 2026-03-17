45eb503b0d248f6f54ec574877d128311a097cb3
totSamiy73 <dmitriy73a@mail.ru>
2026-03-17 14:05:44 +0300

    homework_10 исправления по заданию 2 (Code Style) и 4 (не выполненное условие, тип int для значений ключа)

--- homework/d.akulov/lesson_10/task2.py
+++ homework/d.akulov/lesson_10/task2.py
@@ -1,7 +1,7 @@
 # вариант 1
 def repeat_me1(func):
     def wrapper(text, count):
-        for i in range(count):
+        for _ in range(count):
             func(text)
         print("finished 1 test")
 
@@ -22,7 +22,7 @@ def repeat_me2(**kwargs):
 
     def decoration(fun):
         def wrapper(text):
-            for i in range(count):
+            for _ in range(count):
                 fun(text)
             print("finished 2 test")
 
--- homework/d.akulov/lesson_10/task4.py
+++ homework/d.akulov/lesson_10/task4.py
@@ -8,6 +8,7 @@ PRICE_LIST = '''тетрадь 50р
 
 PRICE_LIST = PRICE_LIST.split()
 
-print({PRICE_LIST[e]: PRICE_LIST[e + 1] for e in range(0, len(PRICE_LIST) - 1, 2)})
+# про инт упустил-подправил, а дважды печатаем это типо 2 разных кривых варианта))
+print({PRICE_LIST[e]: int(PRICE_LIST[e + 1][:-1]) for e in range(0, len(PRICE_LIST) - 1, 2)})
 
-print(dict(zip(PRICE_LIST[::2], PRICE_LIST[1::2])))
+print(dict(zip(PRICE_LIST[::2], [int(e[:-1]) for e in PRICE_LIST[1::2]])))
