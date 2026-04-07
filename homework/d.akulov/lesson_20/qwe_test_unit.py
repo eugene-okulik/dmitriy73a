3ca88a477f279a6dcd370c9f37c526eeb64944fd
totSamiy73 <dmitriy73a@mail.ru>
2026-04-07 12:01:34 +0300

    homework_20 lint

--- homework/d.akulov/lesson_20/qwe_test_unit.py
+++ homework/d.akulov/lesson_20/qwe_test_unit.py
@@ -11,7 +11,7 @@ class TestApi(unittest.TestCase):
     def setUp(self):
         self.post = requests.post("http://objapi.course.qa-practice.com/object",
                                   json={'data': {'color': 'qqq', 'size': 'zxc'}, 'name': 'test666'})
-        print(f'\nPost created: {self.post.json()['id']}')
+        print(f"\nPost created: {self.post.json()['id']}")
 
     # постусловие, удаляем пользователя
     def tearDown(self):
