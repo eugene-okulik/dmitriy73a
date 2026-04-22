1283928a8a43246d0bd89c4cf55b20d4579d8b46
totSamiy73 <dmitriy73a@mail.ru>
2026-04-22 23:04:53 +0300

    homework_25 +

--- homework/d.akulov/lesson25/task1.py
+++ homework/d.akulov/lesson25/task1.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+
 def test_task1(driver_chrome):
     driver_chrome.get("https://www.qa-practice.com/elements/input/simple")
     input = driver_chrome.find_element(By.XPATH, '//*[@id="id_text_string"]')
@@ -11,7 +12,6 @@ def test_task1(driver_chrome):
     assert element.value_of_css_property("color") == "rgba(255, 0, 0, 1)"
 
 
-
 def test_task1_2(driver_chrome):
     driver_chrome.get("https://www.qa-practice.com/elements/input/simple")
     input = driver_chrome.find_element(By.CSS_SELECTOR, '[id="id_text_string"]')
@@ -20,4 +20,3 @@ def test_task1_2(driver_chrome):
     element = driver_chrome.find_element(By.CLASS_NAME, "result-text")
     assert element.text == "qwerty"
     print(element.text)
-
